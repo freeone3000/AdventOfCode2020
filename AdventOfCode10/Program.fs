0efca0444be4c2e68193c6006f8e9718e27f39d7
James Moore <freeone3000@gmail.com>
2020-12-10 15:46:26 -0500

    Solution to problem 10 part 1.

---
+++ AdventOfCode10/Program.fs
@@ -0,0 +1,31 @@
+﻿open System
+
+let readlines (fn:string) : seq<string> =
+    System.IO.File.ReadLines(fn)
+
+[<EntryPoint>]
+let main argv =
+    let mutable oneDiff = 0
+    let mutable twoDiff = 0
+    let mutable threeDiff = 0
+    
+    readlines argv.[0]
+    |> Seq.map Int32.Parse
+    |> Seq.sort
+    |> Seq.fold (fun last cur ->
+        let diff = cur - last
+        match diff with
+        | 1 -> oneDiff <- oneDiff + 1
+        | 2 -> twoDiff <- twoDiff + 1
+        | 3 -> threeDiff <- threeDiff + 1
+        | _ -> failwith "invalid combination"
+        cur
+        )
+        0
+    |> ignore
+    
+    threeDiff <- threeDiff + 1 // three bigger than largest adapter
+    printfn "Ones:\t%d\nThrees:\t%d\n" oneDiff threeDiff
+    printfn "Result:\t%d" (oneDiff * threeDiff)
+    0
+                
