d5986fbca274573921f3a294181ae0f17719692b
Jasmine Moore <freeone3000@gmail.com>
2021-11-30 03:55:33 -0500

    Completed part2. Only required expanding the number of dimensions, simples.

--- AdventOfCode17/Program.fs
+++ AdventOfCode17/Program.fs
@@ -16,47 +16,48 @@ let ON = 1 // '#'
 let OFF = 0 // '.'
 
 type SmartGrid(initial: int[,]) =
-    let mutable printMinimum = (HORIZON, HORIZON, HORIZON)
-    let mutable printMaximum = (Array2D.length1 initial + HORIZON - 1, Array2D.length2 initial + HORIZON - 1, HORIZON)
+    let mutable printMinimum = (HORIZON, HORIZON, HORIZON, HORIZON)
+    let mutable printMaximum = (Array2D.length1 initial + HORIZON - 1, Array2D.length2 initial + HORIZON - 1, HORIZON, HORIZON)
     let mutable cycleCount = 0
-    let mutable backing = Array3D.zeroCreate (Array2D.length1 initial + HORIZON + HORIZON) (Array2D.length2 initial + HORIZON + HORIZON) (1 + HORIZON + HORIZON)
+    let mutable backing = Array4D.zeroCreate (Array2D.length1 initial + HORIZON + HORIZON) (Array2D.length2 initial + HORIZON + HORIZON) (1 + HORIZON + HORIZON) (1 + HORIZON + HORIZON)
     
     do
-        let bset (x, y, z) value =
-            backing.[x + HORIZON, y + HORIZON, z + HORIZON] <- value
+        let bset (x, y) value =
+            backing.[x + HORIZON, y + HORIZON, HORIZON, HORIZON] <- value
         // initialize and copy
         for x = 0 to Array2D.length1 initial - 1 do
             for y = 0 to Array2D.length2 initial - 1 do
-                    bset (x, y, 0) initial.[x, y]
+                    bset (x, y) initial.[x, y]
     
-    let bget (mx, my, mz) =
-        if (mx >= Array3D.length1 backing || my >= Array3D.length2 backing || mz >= Array3D.length3 backing) || (mx < 0 || my < 0 || mz < 0) then 0
-        else backing.[mx, my, mz]
+    let bget (mx, my, mz, mw) =
+        if (mx >= Array4D.length1 backing || my >= Array4D.length2 backing || mz >= Array4D.length3 backing || mw >= Array4D.length4 backing) || (mx < 0 || my < 0 || mz < 0 || mw < 0) then 0
+        else backing.[mx, my, mz, mw]
         
 
           
     member this.generateInPlace () =
-        let mutable minCoords = (Array3D.length1 backing, Array3D.length2 backing, Array3D.length3 backing)
-        let mutable maxCoords = (-1, -1, -1)
+        let mutable minCoords = (Array4D.length1 backing, Array4D.length2 backing, Array4D.length3 backing, Array4D.length4 backing)
+        let mutable maxCoords = (-1, -1, -1, -1)
         
         cycleCount <- cycleCount + 1 // update here for easier debugging
         
         // utility function
-        let piecewise f (a1, b1, c1) (a2, b2, c2) = (f a1 a2, f b1 b2, f c1 c2)
+        let piecewise f (a1, b1, c1, d1) (a2, b2, c2, d2) = (f a1 a2, f b1 b2, f c1 c2, f d1 d2)
         
-        let successorFunction x y z =
+        let successorFunction x y z w =
             // sum neighbors
-            let neighbors x y z =
+            let neighbors x y z w =
                 let mutable sum = 0
                 for xoff = -1 to 1 do
                     for yoff = -1 to 1 do
                         for zoff = -1 to 1 do
-                            if xoff <> 0 || yoff <> 0 || zoff <> 0 then
-                                sum <- sum + bget (x+xoff, y+yoff, z+zoff)
+                            for woff = -1 to 1 do
+                                if xoff <> 0 || yoff <> 0 || zoff <> 0 || woff <> 0 then
+                                    sum <- sum + bget (x+xoff, y+yoff, z+zoff, w+woff)
                 sum
             // calculate new status
-            let ncount = neighbors x y z
-            let prev = bget (x, y, z)
+            let ncount = neighbors x y z w
+            let prev = bget (x, y, z, w)
             let newStatus = 
                 if prev = ON then
                     if (ncount = 2 || ncount = 3) then ON else OFF
@@ -64,37 +65,39 @@ type SmartGrid(initial: int[,]) =
                     if ncount = 3 then ON else OFF
             // update the min/max for print later
             if newStatus = ON then
-                minCoords <- piecewise min minCoords (x, y, z)
-                maxCoords <- piecewise max maxCoords (x, y, z)
+                minCoords <- piecewise min minCoords (x, y, z, w)
+                maxCoords <- piecewise max maxCoords (x, y, z, w)
             // return
             newStatus
         
-        let newItem = Array3D.init (Array3D.length1 backing) (Array3D.length2 backing) (Array3D.length2 backing) successorFunction
+        let newItem = Array4D.init (Array4D.length1 backing) (Array4D.length2 backing) (Array4D.length3 backing) (Array4D.length4 backing) successorFunction
         printMinimum <- minCoords
         printMaximum <- maxCoords
         backing <- newItem
 
     member this.print () =
-        let (minX, minY, minZ) = printMinimum
-        let (maxX, maxY, maxZ) = printMaximum
+        let (minX, minY, minZ, minW) = printMinimum
+        let (maxX, maxY, maxZ, maxW) = printMaximum
         
         printfn "Starting at (%d, %d)" minX minY
         
         printfn "After %d cycles:" cycleCount
-        for z = minZ to maxZ do
-            printfn "\nz=%d" (z - HORIZON) // by convention
-            for x = minX to maxX do
-                for y = minY to maxY do
-                    printf (if backing.[x, y, z] = 1 then "#" else ".")
-                printfn ""
+        for w = minW to maxW do
+            for z = minZ to maxZ do
+                printfn "\nz=%d, w=%d" (z - HORIZON) (w - HORIZON) // by convention
+                for x = minX to maxX do
+                    for y = minY to maxY do
+                        printf (if backing.[x, y, z, w] = 1 then "#" else ".")
+                    printfn ""
         printfn "\n"
         
     member this.Count () =
         let mutable sum = 0
-        for x = 0 to Array3D.length1 backing - 1 do
-            for y = 0 to Array3D.length2 backing - 1 do
-                for z = 0 to Array3D.length3 backing - 1 do
-                    if backing.[x, y, z] = 1 then sum <- sum + 1
+        for x = 0 to Array4D.length1 backing - 1 do
+            for y = 0 to Array4D.length2 backing - 1 do
+                for z = 0 to Array4D.length3 backing - 1 do
+                    for w = 0 to Array4D.length4 backing - 1 do
+                        sum <- sum + backing.[x, y, z, w]
         sum
 
 let readFileToGrid (fn: string) : int[,] =
@@ -111,7 +114,7 @@ let simulate fn =
 [<EntryPoint>]
 let main argv =
     // test case
-    assert ((simulate "input17_sample.txt") = 112)
+    assert ((simulate "input17_sample.txt") = 848)
     // main program execution
     let solution = simulate "input17.txt"
     printfn "Count after %d: %d" CYCLES solution 
